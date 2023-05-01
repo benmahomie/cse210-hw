6cd9b45ef6d8687f0ed88cdb77f0c9f838a3139b
Ben <benmahoney2000@gmail.com>
2023-05-01 12:37:05 -0600

    Finished Learning02

---
+++ abstract-example/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn","Brandon Sanderson");
+
+mistborn.Display();
+Console.WriteLine(mistborn.IsAvailable());
+
+Book bookOfMormon = new Book("The Book of Mormnon","Various Authors");
+
+bookOfMormon.Display();
+Console.WriteLine(bookOfMormon.IsAvailable());

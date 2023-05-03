6267b57099bdf254578fb98414f1c22d77fa6a77
Ben <benmahoney2000@gmail.com>
2023-05-03 12:41:29 -0600

    csv stuff

---
+++ abstract-example/Prompt.cs
@@ -0,0 +1,15 @@
+public class Prompt
+{
+    List<string> _usedPrompts = new List<string>();
+    List<string> _prompts = new List<string>();
+
+    public Prompt()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.IO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        DateTime date = DateTime.Now;
+        string dateString = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+        string destPath = "journals\\journal.csv";
+        string[] headerRow = {"Prompt", "Entry", "Date"};
+
+        void AppendToCsv(string destPath, string[] newRow)
+        {
+            if (!File.Exists(destPath))
+            {
+                Console.WriteLine($"No file called '{destPath}'");
+            }
+
+            using (StreamWriter sw = File.AppendText(destPath))
+            {
+                sw.WriteLine(string.Join(",", newRow));
+            }
+        }
+
+        Console.WriteLine(dateString);
+        string prompt = "How was your day?";
+        Console.WriteLine(prompt);
+        string entry = Console.ReadLine();
+
+        string[] newRow = {prompt, entry, dateString};
+
+        AppendToCsv(destPath, newRow);
     }
 }

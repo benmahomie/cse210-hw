3b195109a6109ec7b6578574a18d3c249c10123a
Ben <benmahoney2000@gmail.com>
2023-05-17 18:27:14 -0600

    Learning03 Complete and tested

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,34 @@
-using System;
+Fraction first = new Fraction();
+Fraction second = new Fraction(6);
+Fraction third = new Fraction(6, 7);
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
-    }
-}
+Console.WriteLine("");
+Console.WriteLine("Display the results as fractions:");
+Console.WriteLine(first.GetFractionString());
+Console.WriteLine(second.GetFractionString());
+Console.WriteLine(third.GetFractionString());
+
+Console.WriteLine("");
+Console.WriteLine("Display the results as decimals:");
+Console.WriteLine(first.GetDecimalValue());
+Console.WriteLine(second.GetDecimalValue());
+Console.WriteLine(third.GetDecimalValue());
+
+Console.WriteLine("");
+Console.WriteLine("Change something from each object using SetBottom() and/or SetTop()...");
+first.SetTop(2);
+second.SetBottom(6);
+third.SetTop(1);
+third.SetBottom(2);
+
+Console.WriteLine("");
+Console.WriteLine("Display the results as fractions:");
+Console.WriteLine(first.GetFractionString());
+Console.WriteLine(second.GetFractionString());
+Console.WriteLine(third.GetFractionString());
+
+Console.WriteLine("");
+Console.WriteLine("Display the new results as decimals:");
+Console.WriteLine(first.GetDecimalValue());
+Console.WriteLine(second.GetDecimalValue());
+Console.WriteLine(third.GetDecimalValue());

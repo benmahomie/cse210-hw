6a72dc864415ae8ed42efad491554e5477035f05
Ben <benmahoney2000@gmail.com>
2023-05-31 23:29:23 -0600

    learning 03

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,8 @@
+public class Assignments {
+    private string _studentName;
+    private string _topic;
+
+    public void GetSummary() {
+        Console.WriteLine($"{_studentName} :: {_topic}");
+    }
+}
